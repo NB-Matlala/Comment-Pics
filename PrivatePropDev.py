8f82102145dd09fdb9cf76fba2b4a89e8bded914
NB Matlala <njabulo_matlala@yahoo.com>
2024-07-25 16:12:29 +0200

    Update PrivatePropDev.py

--- PrivatePropDev.py
+++ PrivatePropDev.py
@@ -277,9 +277,6 @@ async def main2():
         blob_client.upload_blob(data, overwrite=True)
         print(f"Inside File uploaded to Azure Blob Storage: {blob_name}")
 
-    body = ''
-    requests.post('https://prod2-31.southafricanorth.logic.azure.com:443/workflows/85956207b15d4806af09d4ed0d78a404/triggers/When_a_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=9wSTRAla28G_81SoFVz14lbacRE14PJMRmjjKpo3v_I', json=body)        
-
 
 
 # Running the main coroutine
