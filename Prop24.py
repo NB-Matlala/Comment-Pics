bcb8a0042513e0d2b93cc41263c9cfd8355d5ad3
NB Matlala <njabulo_matlala@yahoo.com>
2024-07-29 20:49:06 +0200

    Update Prop24.py

--- Prop24.py
+++ Prop24.py
@@ -1,23 +1,24 @@
 from requests_html import HTMLSession
-import re
 from bs4 import BeautifulSoup
-import json
-import random
-import csv
 import math
+import random
 from datetime import datetime
-from azure.storage.blob import BlobClient
 import time
+import csv
+from azure.storage.blob import BlobClient
 
-######################################Functions##########################################################
+session = HTMLSession()
+
+start_page = 1
+
 def getPages(soup):
     try:
-        num_listings = soup.find('div', class_='pull-right').find_all('span', class_='p24_bold')
+        num_listings = soup.find('div',class_='pull-right').find_all('span', class_='p24_bold')
         num_pgV = num_listings[-1].text.strip()
         pages = math.ceil(int(num_pgV) / 20)
         return pages
     except (ValueError, AttributeError) as e:
-        print(f"Failed to parse number of pages - {e}")
+        print(f"Failed to parse number of pages for URL: {url} - {e}")
         return 0
 
 def getIDs_create_url(soup):
@@ -26,34 +27,33 @@ def getIDs_create_url(soup):
     if p24_results:
         col_9_div = p24_results.find('div', class_='col-9')
         if col_9_div:
-            tile_containers = col_9_div.find_all('div', class_='p24_tileContainer')
-            for tile in tile_containers:
-                listing_number = tile.get('data-listing-number')
-                if listing_number:
-                    listing_number = listing_number.replace('P', '')
-                    thread_data.append(listing_number)
-    # Create URLs
-    links = [f"https://www.property24.com/for-sale/langebaan-country-estate/langebaan/western-cape/10483/{i}?" for i in thread_data]
+           tile_containers = col_9_div.find_all('div', class_='p24_tileContainer')
+           for tile in tile_containers:
+               listing_number = tile.get('data-listing-number')
+               if listing_number:
+                   listing_number = listing_number.replace('P', '')
+                   thread_data.append(listing_number)
+    #create Url
+    links = []
+    for i in thread_data:
+        links.append(f"https://www.property24.com/for-sale/langebaan-country-estate/langebaan/western-cape/10483/{i}?")
     return links
 
 def extractor(soup): # extracts from created urls
-    listing_id = soup.find('div', class_='p24_listing p24_regularListing').get('data-listingnumber')
+    listing_id = soup.find('div',class_='p24_listing p24_regularListing').get('data-listingnumber')
     try:
         comment_24 = soup.find('div', class_='js_expandedText p24_expandedText hide')
         prop_desc = ' '.join(comment_24.stripped_strings)
     except:
         prop_desc = None
-
     current_datetime = datetime.now().strftime('%Y-%m-%d')
-
-    return {
-        "Listing ID": listing_id, "Description": prop_desc, "Time_stamp": current_datetime}
+    return {"Listing ID": listing_id, "Description": prop_desc, "Time_stamp": current_datetime}
 
 def extractor_pics(soup): # extracts from created urls
-    listing_id = soup.find('div', class_='p24_listing p24_regularListing').get('data-listingnumber')
+    listing_id = soup.find('div',class_='p24_listing p24_regularListing').get('data-listingnumber')
     photo_data = []
     try:
-        photogrid_div = soup.find('div', class_='p24_mediaHolder hide').find('div', class_='p24_thumbnailContainer').find_all('div', class_='col-4 p24_galleryThumbnail')
+        photogrid_div = soup.find('div',class_='p24_mediaHolder hide').find('div',class_='p24_thumbnailContainer').find_all('div',class_='col-4 p24_galleryThumbnail')
         if photogrid_div:
             for x in photogrid_div:
                 photo_url = x.find('img').get('lazy-src')
@@ -62,89 +62,76 @@ def extractor_pics(soup): # extracts from created urls
         print(f"No picture div found: {listing_id}")
     return photo_data
 
-######################################Functions##########################################################
-def main():
-    fieldnames = ['Listing ID', 'Description', 'Time_stamp']
-    filename = "Prop24Comments2.csv"
-
-    fieldnames_pics = ['Listing_ID', 'Photo_Link']
-    filename_pics = "Prop24Pictures2.csv"
-
-    session = HTMLSession()
-
-    connection_string = "DefaultEndpointsProtocol=https;AccountName=privateproperty;AccountKey=zX/k04pby4o1V9av1a5U2E3fehg+1bo61C6cprAiPVnql+porseL1NVw6SlBBCnVaQKgxwfHjZyV+AStKg0N3A==;BlobEndpoint=https://privateproperty.blob.core.windows.net/;QueueEndpoint=https://privateproperty.queue.core.windows.net/;TableEndpoint=https://privateproperty.table.core.windows.net/;FileEndpoint=https://privateproperty.file.core.windows.net/;"
-    container_name = "comments-pics"
-    blob_name_comments = "Prop24Comments2.csv"
-    blob_name_pics = "Prop24Pictures2.csv"
-
-    blob_client_comments = BlobClient.from_connection_string(connection_string, container_name, blob_name_comments)
-    blob_client_pics = BlobClient.from_connection_string(connection_string, container_name, blob_name_pics)
-
-    with open(filename, 'w', newline='', encoding='utf-8-sig') as csvfile, \
-         open(filename_pics, 'w', newline='', encoding='utf-8-sig') as csvfile_pics:
-
+def append_to_blob_and_reset(local_file, blob_client, fieldnames):
+    with open(local_file, "rb") as data:
+        blob_client.append_block(data.read())
+    with open(local_file, 'w', newline='', encoding='utf-8-sig') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer_pics = csv.DictWriter(csvfile_pics, fieldnames=fieldnames_pics)
-
         writer.writeheader()
-        writer_pics.writeheader()
 
-        start_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+home_page = session.get('https://www.property24.com/for-sale/advanced-search/results/?sp=pid%3d5%2c6%2c9%2c7%2c8%2c1%2c14%2c2%2c3')
+home_soup = BeautifulSoup(home_page.content, 'html.parser')
+pages = getPages(home_soup)
 
-        home_page = session.get('https://www.property24.com/for-sale/advanced-search/results/?sp=pid%3d5%2c6%2c9%2c7%2c8%2c1%2c14%2c2%2c3')
-        home_soup = BeautifulSoup(home_page.content, 'html.parser')
-        pages = getPages(home_soup)
-        extract_links = []
+connection_string = "##"
+container_name = "comments-pics"
+blob_name_comments = "Prop24Comments2.csv"
+blob_name_pics = "Prop24Pictures2.csv"
 
-        for pg in range(1, pages + 1):
-            link = f"https://www.property24.com/for-sale/advanced-search/results/p{pg}?sp=pid%3d5%2c6%2c9%2c7%2c8%2c1%2c14%2c2%2c3"
-            home = session.get(link)
-            soup = BeautifulSoup(home.content, 'html.parser')
-            extract_links.extend(getIDs_create_url(soup))
-            if pg % 200 == 0:
-                print("Sleeping for 60 seconds after 200 pages...")
-                time.sleep(60)
+blob_client_comments = BlobClient.from_connection_string(connection_string, container_name, blob_name_comments)
+blob_client_pics = BlobClient.from_connection_string(connection_string, container_name, blob_name_pics)
 
-        count = 0
-        print(f"IDs Extracted: {len(extract_links)}")
-        for l in extract_links:
-            count += 1
-            if count % 20 == 0:
-                time.sleep(50)
+fieldnames_comments = ['Listing ID', 'Description', 'Time_stamp']
+filename_comments = "Prop24Comments2.csv"
 
-            home_ex = session.get(l)
-            soupex = BeautifulSoup(home_ex.content, 'html.parser')
+fieldnames_pics = ['Listing_ID', 'Photo_Link']
+filename_pics = "Prop24Pictures2.csv"
 
-            try:
-                comments = extractor(soupex)
-                photos = extractor_pics(soupex)
+with open(filename_comments, 'w', newline='', encoding='utf-8-sig') as csvfile_comments, \
+     open(filename_pics, 'w', newline='', encoding='utf-8-sig') as csvfile_pics:
 
-                writer.writerow(comments)
-                for photo in photos:
-                    writer_pics.writerow(photo)
+    writer_comments = csv.DictWriter(csvfile_comments, fieldnames=fieldnames_comments)
+    writer_pics = csv.DictWriter(csvfile_pics, fieldnames=fieldnames_pics)
 
-                if count % 2000 == 0:
-                    # Append the content of the local CSV files to the blob storage
-                    with open(filename, "rb") as data:
-                        blob_client_comments.append_block(data.read())
-                    with open(filename_pics, "rb") as data_pics:
-                        blob_client_pics.append_block(data_pics.read())
-                    
-                    # Clear the CSV files after appending the content to avoid duplicates
-                    with open(filename, 'w', newline='', encoding='utf-8-sig') as csvfile:
-                        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                        writer.writeheader()
+    writer_comments.writeheader()
+    writer_pics.writeheader()
 
-                    with open(filename_pics, 'w', newline='', encoding='utf-8-sig') as csvfile_pics:
-                        writer_pics = csv.DictWriter(csvfile_pics, fieldnames=fieldnames_pics)
-                        writer_pics.writeheader()
+    count = 0
 
-            except Exception as e:
-                print(f"Error: {l}, {e}")
+    for pg in range(start_page, pages + 1):
+        link = f"https://www.property24.com/for-sale/advanced-search/results/p{pg}?sp=pid%3d5%2c6%2c9%2c7%2c8%2c1%2c14%2c2%2c3"
+        home = session.get(link)
+        soup = BeautifulSoup(home.content, 'html.parser')
+        extract_links = getIDs_create_url(soup)
+        if pg % 200 == 0:
+            print("Sleeping for 60 seconds after processing 200 pages...")
+            time.sleep(60)
 
-        end_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        print(f"Start Time: {start_time}")
-        print(f"End Time: {end_time}")
+    print(f"links extracted: {len(extract_links)}")
+    for l in extract_links:
+        count += 1
+        if count % 10 == 0:
+            sleep_duration = random.randint(35, 50)
+            time.sleep(sleep_duration)
 
-if __name__ == "__main__":
-    main()
+        home_page = session.get(l)
+        soupex = BeautifulSoup(home_page.content, 'html.parser')
+        try:
+            comments = extractor(soupex)
+            photos = extractor_pics(soupex)
+
+            writer_comments.writerow(comments)
+            for photo in photos:
+                writer_pics.writerow(photo)
+            
+            if count % 2000 == 0:
+                append_to_blob_and_reset(filename_comments, blob_client_comments, fieldnames_comments)
+                append_to_blob_and_reset(filename_pics, blob_client_pics, fieldnames_pics)
+
+        except Exception as e:
+            print(f"Error: {l}, {e}")
+
+
+    # Append remaining data at the end
+    append_to_blob_and_reset(filename_comments, blob_client_comments, fieldnames_comments)
+    append_to_blob_and_reset(filename_pics, blob_client_pics, fieldnames_pics)
