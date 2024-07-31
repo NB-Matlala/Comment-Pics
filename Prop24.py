8d796a74c2a674cefb0255038c05fb5c12954fcc
NB Matlala <njabulo_matlala@yahoo.com>
2024-07-31 11:25:38 +0200

    Update Prop24.py22

--- Prop24.py
+++ Prop24.py
@@ -12,10 +12,6 @@ from datetime import datetime, timedelta
 import time
 
 from datetime import datetime
-import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
-from sqlalchemy import create_engine, text
-import pyodbc
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
