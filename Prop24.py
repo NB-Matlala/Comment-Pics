b647a870330404aa82532bf2e59b8e66a5fc74a7
NB Matlala <njabulo_matlala@yahoo.com>
2024-07-31 11:23:01 +0200

    Update Prop24.py

--- Prop24.py
+++ Prop24.py
@@ -1,136 +1,253 @@
-from requests_html import HTMLSession
+
+print("Buy inside code running......................")
+
+import aiohttp
+import asyncio
 from bs4 import BeautifulSoup
-import math
-import random
+import datetime
+import pandas as pd
+import requests
+from azure.storage.blob import BlobClient
+from datetime import datetime, timedelta
+import time
+
+from datetime import datetime
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+from sqlalchemy import create_engine, text
+import pyodbc
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
+    'Accept-Language': 'en-US,en;q=0.5'
+}
+
+start_page = 12100
+last_page = 14200
+pages_per_batch = 101
+thread_data = []
+failed_pages = []  # List to store failed pages
+tile_urls_with_fake_class = []
+
+async def scrape_page(session, url, page):
+    try:
+        async with session.get(url, headers=headers, timeout=70) as response:
+            if response.status == 200:
+                page_content = await response.text()
+                soup = BeautifulSoup(page_content, 'html.parser')
+
+                # Find all <style> tags with type="text/css"
+                style_tags = soup.find_all('style', type='text/css')
+
+                # Iterate over each style tag to find the class name after the <style type="text/css"> tag
+                fake_class_names = []
+                # Iterate over each style tag
+                for style_tag in style_tags:
+                    # Extract the content of the style tag
+                    style_content = style_tag.text.strip()
+
+                    # Split the content by lines and iterate over lines
+                    for line in style_content.splitlines():
+                        # Strip any leading/trailing whitespace
+                        line = line.strip()
+
+                        # Check if the line starts with a '.' (indicating a class definition)
+                        if line.startswith('.'):
+                            # Extract the class name by splitting on '{' and taking the first part
+                            class_name = line.split('{')[0].strip()
+
+                            # Remove the leading '.' from the class name
+                            class_name = class_name[1:]  # Remove the leading '.'
+
+                            # Append the class name to the list
+                            fake_class_names.append(class_name)
+                            break
+
+                # Print out the extracted fake class names
+                #print("Fake Class Names Found:")
+                #for class_name in fake_class_names:
+                #    print(class_name)
+
+
+                p24_results = soup.find('div', class_='p24_results')
+                if p24_results:
+                    col_9_div = p24_results.find('div', class_='col-9')
+                    #if col_9_div:
+                    #    tile_containers = col_9_div.find_all('div', class_='p24_tileContainer')
+                    #    for tile in tile_containers:
+                    #        listing_number = tile.get('data-listing-number')
+                    #        if listing_number:
+                    #            listing_number = listing_number.replace('P', '')
+                    #        thread_data.append(listing_number)
+
+                            #-----------------------------------------
+                    if col_9_div:
+                        tile_containers = col_9_div.find_all('div', class_='p24_tileContainer')
+                        for tile in tile_containers:
+
+                            if any(cls in tile['class'] for cls in fake_class_names):
+                                a_tag = tile.find('a', href=True)
+                                url = a_tag['href'] if a_tag else None
+                                if url is not None:
+                                    listing_number ="https://www.property24.com"+url
+                                    #print("urls:",listing_number)
+                                    tile_urls_with_fake_class.append(listing_number)
+                                    #print("fake url",tile_urls_with_fake_class)
+
+                            else:
+                                a_tag = tile.find('a', href=True)
+                                url = a_tag['href'] if a_tag else None
+                                if url is not None:
+                                    listing_number ="https://www.property24.com"+url
+                                    #print("urls:",listing_number)
+                                    thread_data.append(listing_number)
+
+            else:
+                #print(f"Failed to retrieve page {page}: Status code {response.status}")
+                failed_pages.append(page)
+    except Exception as e:
+        #print(f"An error occurred while scraping page {page}: {e}")
+        failed_pages.append(page)
+
+async def scrape_batch(session, start, end):
+    tasks = []
+    for page in range(start, end + 1):
+        url = f"https://www.property24.com/for-sale/advanced-search/results/p{page}?sp=pid%3d8%2c2%2c3%2c14%2c5%2c1%2c6%2c9%2c7%26so%3dNewest&PropertyCategory=House%2cApartmentOrFlat%2cTownhouse%2cVacantLandOrPlot%2cFarm%2cCommercial%2cIndustrial"
+        tasks.append(scrape_page(session, url, page))
+    await asyncio.gather(*tasks)
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        for start in range(start_page, last_page + 1, pages_per_batch):
+            end = min(start + pages_per_batch - 1, last_page)
+            await scrape_batch(session, start, end)
+            if end < last_page:
+                #print(f"Sleeping for 60 seconds before scraping pages {end + 1} to {min(end + pages_per_batch, last_page)}")
+                await asyncio.sleep(60)
+        global failed_pages
+        # Scrape failed pages as a new batch
+        if failed_pages:
+            failed_pages = list(set(failed_pages))
+            #print(f"Re-scraping failed pages: {failed_pages}")
+            await scrape_batch(session, min(failed_pages), max(failed_pages))
+
+if __name__ == "__main__":
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    thread_data = [item for item in thread_data if item is not None]
+    thread_data = list(set(thread_data))
+    #print(thread_data)
+    #print(len(thread_data))
+    data_frame = pd.DataFrame(thread_data)
+    data_frame.to_csv('property_listingsIDs.csv', encoding='utf-8', index=False)
+
+    data_frame2 = pd.DataFrame(tile_urls_with_fake_class, columns=['URLs'])
+    data_frame2.to_csv('property_listingsfakeads.csv', encoding='utf-8', index=False)
+
+time.sleep(30)
+
+import aiohttp
+import asyncio
+import pandas as pd
+from bs4 import BeautifulSoup
 from datetime import datetime
 import time
-import csv
-from azure.storage.blob import BlobClient
 
-session = HTMLSession()
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
+    'Accept-Language': 'en-US,en;q=0.5'
+}
+base_url = "https://www.property24.com/for-sale/uvongo/margate/kwazulu-natal/6359/{id}"
+def get_text_or_none(element):
+    return element.get_text(strip=True) if element else None
 
-start_page = 1
-
-def getPages(soup):
+async def extract_property_details(session, listing_id):
+    url = listing_id
     try:
-        num_listings = soup.find('div',class_='pull-right').find_all('span', class_='p24_bold')
-        num_pgV = num_listings[-1].text.strip()
-        pages = math.ceil(int(num_pgV) / 20)
-        return pages
-    except (ValueError, AttributeError) as e:
-        print(f"Failed to parse number of pages for URL - {e}")
-        return 0
+        async with session.get(url, timeout=70) as response:
+            if response.status != 200:
+                print(f"respomse status:{response.status}")
 
-def getIDs_create_url(soup):
-    thread_data = []
-    p24_results = soup.find('div', class_='p24_results')
-    if p24_results:
-        col_9_div = p24_results.find('div', class_='col-9')
-        if col_9_div:
-           tile_containers = col_9_div.find_all('div', class_='p24_tileContainer')
-           for tile in tile_containers:
-               listing_number = tile.get('data-listing-number')
-               if listing_number:
-                   listing_number = listing_number.replace('P', '')
-                   thread_data.append(listing_number)
-    #create Url
-    links = []
-    for i in thread_data:
-        links.append(f"https://www.property24.com/for-sale/langebaan-country-estate/langebaan/western-cape/10483/{i}?")
-    return links
+            if response.status == 200:
+                page_content = await response.text()
+                soup = BeautifulSoup(page_content, 'html.parser')
 
-def extractor(soup): # extracts from created urls
-    listing_id = soup.find('div',class_='p24_listing p24_regularListing').get('data-listingnumber')
-    try:
-        comment_24 = soup.find('div', class_='js_expandedText p24_expandedText hide')
-        prop_desc = ' '.join(comment_24.stripped_strings)
-    except:
-        prop_desc = None
-    current_datetime = datetime.now().strftime('%Y-%m-%d')
-    return {"Listing ID": listing_id, "Description": prop_desc, "Time_stamp": current_datetime}
-
-def extractor_pics(soup): # extracts from created urls
-    listing_id = soup.find('div',class_='p24_listing p24_regularListing').get('data-listingnumber')
-    photo_data = []
-    try:
-        photogrid_div = soup.find('div',class_='p24_mediaHolder hide').find('div',class_='p24_thumbnailContainer').find_all('div',class_='col-4 p24_galleryThumbnail')
-        if photogrid_div:
-            for x in photogrid_div:
-                photo_url = x.find('img').get('lazy-src')
-                photo_data.append({'Listing_ID': listing_id, 'Photo_Link': photo_url})
-    except:
-        print(f"No picture div found: {listing_id}")
-    return photo_data
-
-def append_to_blob_and_reset(local_file, blob_client, fieldnames):
-    with open(local_file, "rb") as data:
-        blob_client.upload_blob(data, overwrite=True)
-    with open(local_file, 'w', newline='', encoding='utf-8-sig') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-
-home_page = session.get('https://www.property24.com/for-sale/advanced-search/results/?sp=pid%3d5%2c6%2c9%2c7%2c8%2c1%2c14%2c2%2c3')
-home_soup = BeautifulSoup(home_page.content, 'html.parser')
-pages = getPages(home_soup)
-
-connection_string = "DefaultEndpointsProtocol=https;AccountName=privateproperty;AccountKey=zX/k04pby4o1V9av1a5U2E3fehg+1bo61C6cprAiPVnql+porseL1NVw6SlBBCnVaQKgxwfHjZyV+AStKg0N3A==;BlobEndpoint=https://privateproperty.blob.core.windows.net/;QueueEndpoint=https://privateproperty.queue.core.windows.net/;TableEndpoint=https://privateproperty.table.core.windows.net/;FileEndpoint=https://privateproperty.file.core.windows.net/;"
-container_name = "comments-pics"
-blob_name_comments = "Prop24Comments.csv"
-blob_name_pics = "Prop24Pictures.csv"
-
-blob_client_comments = BlobClient.from_connection_string(connection_string, container_name, blob_name_comments)
-blob_client_pics = BlobClient.from_connection_string(connection_string, container_name, blob_name_pics)
-
-fieldnames_comments = ['Listing ID', 'Description', 'Time_stamp']
-filename_comments = "Prop24Comments.csv"
-
-fieldnames_pics = ['Listing_ID', 'Photo_Link']
-filename_pics = "Prop24Pictures.csv"
-
-with open(filename_comments, 'w', newline='', encoding='utf-8-sig') as csvfile_comments, \
-     open(filename_pics, 'w', newline='', encoding='utf-8-sig') as csvfile_pics:
-
-    writer_comments = csv.DictWriter(csvfile_comments, fieldnames=fieldnames_comments)
-    writer_pics = csv.DictWriter(csvfile_pics, fieldnames=fieldnames_pics)
-
-    writer_comments.writeheader()
-    writer_pics.writeheader()
-
-    count = 0
-    extract_links = []
-    for pg in range(start_page, pages + 1):
-        link = f"https://www.property24.com/for-sale/advanced-search/results/p{pg}?sp=pid%3d5%2c6%2c9%2c7%2c8%2c1%2c14%2c2%2c3"
-        home = session.get(link)
-        soup = BeautifulSoup(home.content, 'html.parser')
-        extract_links.extend(getIDs_create_url(soup))
-        if pg % 200 == 0:
-            print("Sleeping for 60 seconds after processing 200 pages...")
-            time.sleep(60)
-
-    for l in extract_links:
-        count += 1
-        if count % 10 == 0:
-            sleep_duration = random.randint(35, 50)
-            time.sleep(sleep_duration)
-
-        home_page = session.get(l)
-        soupex = BeautifulSoup(home_page.content, 'html.parser')
-        try:
-            comments = extractor(soupex)
-            photos = extractor_pics(soupex)
-
-            writer_comments.writerow(comments)
-            for photo in photos:
-                writer_pics.writerow(photo)
-            
-            if count % 2000 == 0:
-                append_to_blob_and_reset(filename_comments, blob_client_comments, fieldnames_comments)
-                append_to_blob_and_reset(filename_pics, blob_client_pics, fieldnames_pics)
-
-        except Exception as e:
-            print(f"Error: {l}, {e}")
+                listing_id = soup.find('div',class_='p24_listing p24_regularListing').get('data-listingnumber')
+                photo_data = []
+                try:
+                    photogrid_div = soup.find('div',class_='p24_mediaHolder hide').find('div',class_='p24_thumbnailContainer').find_all('div',class_='col-4 p24_galleryThumbnail')
+                    if photogrid_div:
+                        for x in photogrid_div:
+                            photo_url = x.find('img').get('lazy-src')
+                            photo_data.append({'Listing_ID': listing_id, 'Photo_Link': photo_url})
+                except:
+                    print(f"No picture div found: {listing_id}")
 
 
-    # Append remaining data at the end
-    append_to_blob_and_reset(filename_comments, blob_client_comments, fieldnames_comments)
-    append_to_blob_and_reset(filename_pics, blob_client_pics, fieldnames_pics)
+                listing_id = soup.find('div',class_='p24_listing p24_regularListing').get('data-listingnumber')
+                try:
+                    comment_24 = soup.find('div', class_='js_expandedText p24_expandedText hide')
+                    prop_desc = ' '.join(comment_24.stripped_strings)
+                except:
+                    prop_desc = None
+                current_datetime = datetime.now().strftime('%Y-%m-%d')
+                data_desc= {"Listing ID": listing_id,
+                            "Description": prop_desc,
+                            "Time_stamp": current_datetime}
+
+                return photo_data,data_desc
+            else:
+                return None
+    except Exception as e:
+        return None
+
+async def scrape_property_details(listing_ids):
+    prop_desc_list=[]
+    photo_data_list=[]
+
+    async with aiohttp.ClientSession() as session:
+        for index, listing_id in enumerate(listing_ids, start=1):
+            try:
+                #property_details = await extract_property_details(session, listing_id)
+                photo_data,prop_desc = await extract_property_details(session, listing_id)
+                if prop_desc is not None:
+                    prop_desc_list.append(prop_desc)
+
+                if photo_data is not None:
+                    photo_data_list.extend(photo_data)
+
+            except Exception as e:
+                pass
+
+            if index % 200 == 0 or index == len(listing_ids):
+                await asyncio.sleep(60)
+
+
+    import time
+    container_name = "comments-pics"
+    timenow= datetime.now().strftime('%H:%M')
+    filename_comments = f"Prop24Comments2{timenow}.csv"
+    filename_pics = f"Prop24pics2{timenow}.csv"
+
+    data_frame_comments = pd.DataFrame(prop_desc_list)
+    data_frame_photo = pd.DataFrame(photo_data_list)
+    # Save DataFrame to CSV
+    car_data_csv_com = data_frame_comments.to_csv(encoding="utf-8", index=False)
+    car_data_csv_pic = data_frame_photo.to_csv(encoding="utf-8", index=False)
+    connection_string = "DefaultEndpointsProtocol=https;AccountName=privateproperty;AccountKey=zX/k04pby4o1V9av1a5U2E3fehg+1bo61C6cprAiPVnql+porseL1NVw6SlBBCnVaQKgxwfHjZyV+AStKg0N3A==;BlobEndpoint=https://privateproperty.blob.core.windows.net/;QueueEndpoint=https://privateproperty.queue.core.windows.net/;TableEndpoint=https://privateproperty.table.core.windows.net/;FileEndpoint=https://privateproperty.file.core.windows.net/;"
+    client = BlobClient.from_connection_string(conn_str=connection_string, container_name=container_name, blob_name=filename_comments)
+    client.upload_blob(car_data_csv_com, overwrite=True)
+    client = BlobClient.from_connection_string(conn_str=connection_string, container_name=container_name, blob_name=filename_pics)
+    client.upload_blob(car_data_csv_pic, overwrite=True)
+
+
+async def main():
+    global thread_data
+
+    # Scrape property details asynchronously
+    await scrape_property_details(thread_data)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
