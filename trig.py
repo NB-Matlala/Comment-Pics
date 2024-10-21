6c9d24c2683f05d86538b024b76e5b820c19d8f4
NB Matlala <njabulo_matlala@yahoo.com>
2024-10-21 12:20:21 +0200

    Update trig.py

--- trig.py
+++ trig.py
@@ -1,5 +1,5 @@
 import requests
 
 body = '{"Pipeline": 1}'
-requests.post('https://prod2-09.southafricanorth.logic.azure.com:443/workflows/623f55b4346742178048b209a003f895/triggers/When_a_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=PxhThWSLOC3sS3JAg54Z3uZEhTvU9zAbNIhkaAhMPN0', json=body)
+requests.post('https://prod-31.southafricanorth.logic.azure.com:443/workflows/85956207b15d4806af09d4ed0d78a404/triggers/When_a_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=9wSTRAla28G_81SoFVz14lbacRE14PJMRmjjKpo3v_I', json=body)
 print("Request sent.")
